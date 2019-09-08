684a72b93bb07d10102a4bfe665a2433b510a476
sharkovadarya <sharkovadarya@gmail.com>
2019-09-08 23:16:39 +0300

    Updated output.

--- task1/dictionary.py
+++ task1/dictionary.py
@@ -29,18 +29,20 @@ class Dictionary:
             dic_word_len += len(k)
         return dic_word_len / len(self.dict)
 
-    def latin_words_proportion(self):
+    def latin_words_proportion(self, in_collection=False):
         latin_words = 0
         for k, v in self.dict.items():
-            latin_words += v.cnt if all(ord(c) < 128 for c in k) else 0
+            t = v.cnt if in_collection else 1
+            latin_words += t if all(ord(c) < 128 for c in k) else 0
         return latin_words / self.words_cnt
 
-    def stop_words_proportion(self):
+    def stop_words_proportion(self, in_collection=False):
         files = [f for f in os.listdir(STOP_WORDS_FOLDER) if os.path.isfile(os.path.join(STOP_WORDS_FOLDER, f))]
         stop_words = [s.strip() for file in files for s in open(os.path.join(STOP_WORDS_FOLDER, file), "r").readlines()]
         stop_words_count = 0
         for k, v in self.dict.items():
-            stop_words_count += v.cnt if k in stop_words else 0
+            t = v.cnt if in_collection else 1
+            stop_words_count += t if k in stop_words else 0
         return stop_words_count / self.words_cnt
 
 
