c1cbbf21def0c62bb8ef5dac4e8a0d59b0f28e7d
sharkovadarya <sharkovadarya@gmail.com>
2019-10-09 21:04:44 +0300

    Consider only the intersection of the collection and the relevance table.

--- task1/query.py
+++ task1/query.py
@@ -3,4 +3,6 @@ class Query:
         self.id = id
         self.text = text
         self.relevant_test = set()
+        self.irrelevant_test = set()
         self.relevant_train = set()
+        self.irrelevant_train = set()
